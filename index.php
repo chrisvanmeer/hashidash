a49cb74464378c5ea978ed0998c0b5e8b9182daa
Chris van Meer <chris@van-meer.net>
2023-02-21 09:24:32 +0100

    first attempt

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
   $inv = json_decode($_ENV['INVENTORY']);
+  $consul = $_ENV['CONSUL_HTTP_ADDR'];
+
+  $pillars = [
+    "Consul" => $inv->{"consul_servers"}->{'hosts'},
+    "Vault" => $inv->{"vault_servers"}->{'hosts'},
+    "Nomad" => $inv->{"consul_servers"}->{'hosts'},
+    "Docker" => $inv->{"docker_clients"}->{'hosts'}
+  ];
 
 ?>
 
@@ -22,7 +30,7 @@
 <body class="d-flex h-100">
 
   <pre>
-  <?php print_r($inv["consul_clients"]); ?>
+  <?php print_r($inv->{"consul_clients"}); ?>
   </pre>
 
   <div class="container-fluid g-0">
@@ -36,138 +44,38 @@
     <main class="d-flex h-75 align-items-center">
       <div class="container-fluid pt-2 h-100">
         <div class="row d-flex flex-row h-100 text-center">
+
+          <?php foreach ($pillars as $pillar => $servers) { ?>
           <div class="col border-end d-flex flex-column h-100">
             <div class="bg-light fw-medium fs-4 py-2 shadow-sm mb-5 border"><i
-                class="fak fa-consul color-consul me-2"></i>Consul
+                class="fak fa-<?=strtolower($pillar)?> color-<?=strtolower($pillar)?> me-2"></i><?=$pillar?>
             </div>
             <div class="d-flex align-items-start flex-column h-100 align-items-center">
+              <?php foreach ($servers as $server) { ?>
               <div class="d-flex col flex-column text-center">
                 <div class="border rounded p-4 d-flex flex-column server">
                   <span class="fa-stack mb-4">
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="left-13"></i>
+                    <i class="fak fa-<?=strtolower($pillar)?> fa-stack-1x color-gray" data-fa-transform="left-13"></i>
                     <i class="fat fa-server fa-4x"></i>
                   </span>
-                  <span class="mt-1">consul1</span>
+                  <span class="mt-1"><?=$server?></span>
                 </div>
               </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="left-13"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">consul2</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="left-13"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">consul3</span>
-                </div>
-              </div>
+              <?php } ?>
             </div>
           </div>
-          <div class="col border-end d-flex flex-column h-100">
-            <div class="bg-light fw-medium fs-4 py-2 shadow-sm mb-5 border"><i
-                class="fak fa-vault color-vault me-2"></i>Vault</div>
-            <div class="d-flex align-items-start flex-column h-100 align-items-center">
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-vault fa-stack-1x color-gray" data-fa-transform="left-13"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">vault1</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-vault fa-stack-1x color-gray" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">vault2</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-vault fa-stack-1x color-gray" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">vault3</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col border-end d-flex flex-column h-100">
-            <div class="bg-light fw-medium fs-4 py-2 shadow-sm mb-5 border"><i
-                class="fak fa-nomad color-nomad me-2"></i>Nomad</div>
-            <div class="d-flex align-items-start flex-column h-100 align-items-center">
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-nomad fa-stack-1x color-gray" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">nomad1</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-nomad fa-stack-1x color-gray" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">nomad2</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-nomad fa-stack-1x color-gray" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">nomad3</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col d-flex flex-column h-100">
-            <div class="bg-light fw-medium fs-4 py-2 shadow-sm mb-5 border"><i
-                class="fak fa-docker color-docker me-2"></i>Docker</div>
-            <div class="d-flex align-items-start flex-column h-100 align-items-center">
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-docker fa-stack-1x color-docker" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fak fa-nomad fa-stack-1x color-gray" data-fa-transform="up-22 right-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">docker1</span>
-                </div>
-              </div>
-              <div class="d-flex col flex-column text-center">
-                <div class="border rounded p-4 d-flex flex-column server">
-                  <span class="fa-stack mb-4">
-                    <i class="fak fa-docker fa-stack-1x color-docker" data-fa-transform="left-12"></i>
-                    <i class="fak fa-consul fa-stack-1x color-gray" data-fa-transform="up-22 left-40"></i>
-                    <i class="fak fa-nomad fa-stack-1x color-gray" data-fa-transform="up-22 right-40"></i>
-                    <i class="fat fa-server fa-4x"></i>
-                  </span>
-                  <span class="mt-1">docker2</span>
-                </div>
-              </div>
+
+          <?php } ?>
+
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
+
+<!-- 
               <div class="d-flex col flex-column text-center">
                 <div class="border rounded p-4 d-flex flex-column server">
                   <span class="fa-stack mb-4">
@@ -178,13 +86,4 @@
                   </span>
                   <span class="mt-1">docker3</span>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  </div>
-</body>
-
-</html>
+              </div> -->
